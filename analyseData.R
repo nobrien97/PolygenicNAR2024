8d31f1c7cbf3b096296ecd127264a41b1f6cb8fd
nobrien97 <nicholasobrien97@gmail.com>
2024-10-08 16:50:54 +1000

    Update analyseData

--- analyseData.R
+++ analyseData.R
@@ -5,10 +5,10 @@
 
 # Replace these paths with the path to where you saved the 
 # PolygenicNAR2024 repo and where you saved the dataset
-repoPath <- "/path/to/PolygenicNAR2024"
-dataPath <- "/path/to/data/"
+REPO_PATH <- "/path/to/PolygenicNAR2024/"
+DATA_PATH <- "/path/to/data/"
 
-setwd(repoPath)
+setwd(REPO_PATH)
 
 # Load functions
 source("./R/helperFunctionsAndSetup.R")
@@ -17,10 +17,13 @@ source("./R/helperFunctionsAndSetup.R")
 source("./R/wrangle_data.R")
 
 # Analyse epistasis data
-source("./R/mutationScreenExp.R")
+source("./R/epistasis.R")
 
 # Analyse LD data
-source("./R/figureFunctionsAndSetup.R")
+source("./R/LD.R")
 
 # Analyse G matrices
-source("./R/figures.R")
+source("./R/Gmatrix.R")
+
+# Find effect sizes of individual mutations
+source("./R/FX.R")
