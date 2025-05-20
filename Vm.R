2891e9096eb667779760f6235406b5df47a25396
nobrien97 <nicholasobrien97@gmail.com>
2025-05-20 18:16:33 +1000

    Update Vm fig

--- Vm.R
+++ Vm.R
@@ -4,26 +4,28 @@
 d_mutvar <- data.table::fread(paste0(DATA_PATH, "slim_mutvar.csv"), header = F,
                               col.names = c("replicate", "seed", "modelindex", "variance"))
 
+d_mutvar <- d_mutvar %>%
+  mutate(replicate = replicate %/% 2 + 1, # convert replicate id from 1 3 5 to 1 2 3 
+         seed = as.factor(seed),
+         modelindex = as.factor(modelindex))
 
 # Plot Vm in adjusted tau runs (nloci = 1024, tau = 0.0125*scalingFactor)
 # scaling factor given by sensitivity analysis
 d_mutvar_adjtau <- data.table::fread(paste0(DATA_PATH, "slim_mutvar_adjtau.csv"), header = F,
                                      col.names = c("replicate", "seed", "modelindex", "variance"))
 
-d_mutvar_adjtau$normalised <- T
-d_mutvar$normalised <- F
-
 d_mutvar_adjtau <- d_mutvar_adjtau %>%
   mutate(replicate = replicate %/% 2 + 1, # convert replicate id from 1 3 5 to 1 2 3 
          seed = as.factor(seed),
          modelindex = as.factor(modelindex))
 
-d_mutvar_adjtau <- AddCombosToDF(d_mutvar_adjtau)
+d_mutvar_adjtau$normalised <- T
+d_mutvar$normalised <- F
+
 
 # Join with regular
-d_mutvar2 <- full_join(d_mutvar, d_mutvar_adjtau, by = c("replicate", "seed", "modelindex",
-                                                         "normalised", "variance", "model",
-                                                         "nloci", "tau", "r"))
+d_mutvar2 <- full_join(d_mutvar, d_mutvar_adjtau)
+d_mutvar2 <- AddCombosToDF(d_mutvar2)
 
 d_mutvar2 <- d_mutvar2 %>%
   mutate(scaled = if_else(normalised, "Scaled tau", "Unscaled tau"),
@@ -41,7 +43,8 @@ ggplot(d_mutvar2 %>%
   geom_point(data = d_mutvar_sum %>% ungroup() %>%
                mutate(model = fct_recode(model, "Additive" = "Add", "K+" = "K", "K-" = "ODE")), 
              aes(x = interaction(model, scaled), y = meanVar),
-             shape = 3, size = 2, colour = "black", inherit.aes = F) +
+             shape = 3, size = 2, colour = "black", inherit.aes = F,
+             stroke = 1) +
   scale_colour_manual(values = paletteer_d("nationalparkcolors::Everglades", 3, 
                                            direction = -1)) +
   scale_x_discrete(guide = "axis_nested") +
